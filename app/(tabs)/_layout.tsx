d34d50fc722a2cf14331552b6445920dc33707bd
sz237 <sarah.d.zeng@vanderbilt.edu>
2026-02-24 22:12:59 -0600

    (pushing to main since I'm just hiding tabs) Hiding old googleplacesAPI and serpapi tabs from user

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -51,6 +51,18 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="serpapi"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="googleplaces"
+        options={{
+          href: null,
+        }}
+      />
     </Tabs>
   );
 }
